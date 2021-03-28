e304a7a172fef5521675cacfabf1ca8815e6ae63
Kitamura <ymyk6602@gmail.com>
2021-03-28 23:06:59 +0900

    wip

--- src/s5/if_5-2.c
+++ src/s5/if_5-2.c
@@ -2,13 +2,9 @@
 
 
 int main() {
-    int flag = 0;
-    char buf[16];
-
-    scanf("%s", buf);
-    printf("%d\n", flag);
-
-    if (flag != 0) {
+    int a;
+    a = 0;
+    if (a != 0) {
         printf("hacked!\n");
     } else {
         printf("failed!\n");
